184b58bb05c2d4b1889e54f99b8b25dbc4067922
George <george.collingridge@purplebricks.com>
2021-06-01 14:00:10 +0100

    Added hypermedia pagination to api/vehicles endpoint

--- dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
+++ dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Dynamic;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,11 +26,60 @@ namespace Autobarn.Website.Controllers.api
             this.db = db;
         }
 
-        const int PAGE_SIZE = 15;
-        [HttpGet]
-        public IEnumerable<Vehicle> Get(int page)
+        private object Paginate(string href, int index, int count, int total)
         {
-            return db.ListVehicles().Skip(page * PAGE_SIZE).Take(PAGE_SIZE);
+            dynamic links = new ExpandoObject();
+
+            links.self = new
+            {
+                href
+            };
+
+            if (index + count < total)
+            {
+                links.next = new
+                {
+                    href = $"{href}?index={index + count}&count={count}"
+                };
+                links.final = new
+                {
+                    href = $"{href}?index={total - count}"
+
+                };
+            }
+
+            if (index > 0)
+            {
+                links.previous = new
+                {
+                    href = $"{href}?index={index - count}&count={count}"
+
+                };
+                links.first = new
+                {
+                    href = $"{href}?count={count}"
+                };
+            }
+            return links;
+
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Get(int index, int count = 10)
+        {
+            var items = db.ListVehicles().Skip(index).Take(count);
+            var total = db.CountVehicles();
+            var result = new
+            {
+                _links = Paginate("/api/vehicles", index, count, total),
+                total,
+                index,
+                count,
+                items
+            };
+            return Ok(result);
         }
 
         // GET api/vehicles/5
