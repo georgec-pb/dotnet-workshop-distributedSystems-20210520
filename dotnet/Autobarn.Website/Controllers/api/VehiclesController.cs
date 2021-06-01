273ef33867774c379640257cb9240fb42b75d1ee
George Collingridge <george.collingridge@purplebricks.com>
2021-06-01 11:29:57 +0100

    201 rsponses created

--- dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
+++ dotnet/Autobarn.Website/Controllers/api/VehiclesController.cs
@@ -10,60 +10,75 @@ using Autobarn.Website.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Autobarn.Website.Controllers.api {
+namespace Autobarn.Website.Controllers.api
+{
 
-	[Route("api/[controller]")]
-	[ApiController]
-	public class VehiclesController : ControllerBase {
-		private readonly IAutobarnDatabase db;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IAutobarnDatabase db;
 
-		// GET: api/vehicles
-		public VehiclesController(IAutobarnDatabase db) {
-			this.db = db;
-		}
-		[HttpGet]
-		public IEnumerable<Vehicle> Get() {
-			return db.ListVehicles();
-		}
+        // GET: api/vehicles
+        public VehiclesController(IAutobarnDatabase db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public IEnumerable<Vehicle> Get()
+        {
+            return db.ListVehicles();
+        }
 
-		// GET api/vehicles/5
-		[HttpGet("{id}")]
-		public Vehicle Get(string id) {
-			return db.FindVehicle(id);
-		}
+        // GET api/vehicles/5
+        [HttpGet("{id}")]
+        public Vehicle Get(string id)
+        {
+            return db.FindVehicle(id);
+        }
 
-		// POST api/vehicles
-		[HttpPost]
-		public IActionResult Post([FromBody] VehicleDto dto) {
-			var vehicleModel = db.FindModel(dto.ModelCode);
-			var vehicle = new Vehicle {
-				Registration = dto.Registration,
-				Color = dto.Color,
-				Year = dto.Year,
-				VehicleModel = vehicleModel
-			};
-			db.CreateVehicle(vehicle);
-			return Ok(dto);
-		}
+        // POST api/vehicles
+        [HttpPost]
+        public IActionResult Post([FromBody] VehicleDto dto)
+        {
+            // if vehicle already exists , return 409 conflict
+            var existing = db.FindVehicle(dto.Registration);
+            if (existing != default) return Conflict($"Sorry, we already have a car with registration {dto.Registration} in our database.");
 
-		// PUT api/vehicles/5
-		[HttpPut("{id}")]
-		public IActionResult Put(string id, [FromBody] VehicleDto dto) {
-			var vehicleModel = db.FindModel(dto.ModelCode);
-			var vehicle = new Vehicle {
-				Registration = dto.Registration,
-				Color = dto.Color,
-				Year = dto.Year,
-				ModelCode = vehicleModel.Code
-			};
-			db.UpdateVehicle(vehicle);
-			return Ok(dto);
-		}
+            var vehicleModel = db.FindModel(dto.ModelCode);
+            var vehicle = new Vehicle
+            {
+                Registration = dto.Registration,
+                Color = dto.Color,
+                Year = dto.Year,
+                VehicleModel = vehicleModel
+            };
+            db.CreateVehicle(vehicle);
+            return Created($"/api/vehicles/{vehicle.Registration}", dto);
 
-		// DELETE api/vehicles/5
-		[HttpDelete("{id}")]
-		public IActionResult Delete(string id) {
-			return Ok(id);
-		}
-	}
+        }
+
+        // PUT api/vehicles/5
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] VehicleDto dto)
+        {
+            var vehicleModel = db.FindModel(dto.ModelCode);
+            var vehicle = new Vehicle
+            {
+                Registration = dto.Registration,
+                Color = dto.Color,
+                Year = dto.Year,
+                ModelCode = vehicleModel.Code
+            };
+            db.UpdateVehicle(vehicle);
+            return Ok(dto);
+        }
+
+        // DELETE api/vehicles/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            return Ok(id);
+        }
+    }
 }
